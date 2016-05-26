4c24ec847fd3400e9269ba4ef1ec97c1abf59683
shashi <shashi@shashi>
2016-05-27 00:35:19 +0530

    finally added Swwagger to my web Api

--- src/main/java/resource/UserResource.java
+++ src/main/java/resource/UserResource.java
@@ -8,11 +8,16 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
 
 import domain.User;
 import service.UserService;
@@ -22,7 +27,8 @@ import serviceImpl.UserServiceImpl;
  * @author shashi
  *
  */
-@Path("/getUsers")
+@Path("/users")
+@Api(value = "/users", description = "users operations")
 @Produces(javax.ws.rs.core.MediaType.APPLICATION_JSON)
 public class UserResource {
 
@@ -32,17 +38,32 @@ public class UserResource {
 	 */
 
 	@GET
+	@Path("/getUsers")
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "users")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "ok"),
+			@ApiResponse(code = 500, message = "server error") })
 	public Response getUsers(@QueryParam(value = "name") String name)
 			throws JsonParseException, JsonMappingException, IOException {
 		return Response.ok(userService.getUsers(name)).status(200).build();
 	}
 
 	@PUT
+	@Path("/updateUsers")
+	@ApiOperation(value = "users")
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "ok"),
+			@ApiResponse(code = 500, message = "server error") })
 	public boolean updateDesigantion(@QueryParam("designation") String designation) {
 		return userService.updateDesignation(designation);
 	}
 
 	@POST
+	@Path("/createUsers")
+	@ApiOperation(value = "users")
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "ok"),
+			@ApiResponse(code = 500, message = "server error") })
 	public Response createUser(User user) throws JsonProcessingException {
 		Boolean isCreated = userService.createUser(user);
 		System.out.println("created::" + isCreated);
