0b217b5cc9d88897865eea6abb1d8c4e4a80a616
vagrant <vagrant@10.0.2.15>
2016-05-27 15:19:11 +0530

    made couchbase details to get from yaml file

--- src/main/java/couchbase/CouchbaseConnection.java
+++ src/main/java/couchbase/CouchbaseConnection.java
@@ -7,20 +7,22 @@ import java.util.List;
 
 import com.couchbase.client.CouchbaseClient;
 
-import domain.CouchbaseConfiguration;
+import configuration.CouchbaseConfiguration;
 
 /**
  * @author shashi
  *
  */
-public class CouchbaseResource {
+public class CouchbaseConnection {
 
 	public static CouchbaseClient client = null;
 
 	public static CouchbaseClient getClient(CouchbaseConfiguration config) {
 
 		List<URI> uriList = new ArrayList<URI>();
-		uriList.add(URI.create(config.getNode()));
+		for (String uri : config.getNode()) {
+			uriList.add(URI.create(uri));
+		}
 		try {
 			client = new CouchbaseClient(uriList, config.getBucket(), config.getPassword());
 			if (client != null) {
--- src/main/java/couchbase/CouchbaseWrapper.java
+++ src/main/java/couchbase/CouchbaseWrapper.java
@@ -2,13 +2,12 @@ package couchbase;
 
 import java.io.IOException;
 
-import com.couchbase.client.CouchbaseClient;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import domain.CouchbaseConfiguration;
 import domain.User;
+import resource.CouchbaseResource;
 
 /**
  * @author shashi
@@ -16,33 +15,17 @@ import domain.User;
  */
 public class CouchbaseWrapper {
 
-	public static CouchbaseClient client = null;
-	public CouchbaseConfiguration config;
-
-	public CouchbaseWrapper(CouchbaseConfiguration config) {
-		super();
-		this.config = config;
-	}
-
-	public CouchbaseWrapper() {
-		super();
-	}
-
 	public boolean createUser(String id, String result) {
-		if (client == null) {
-			client = CouchbaseResource.getClient(config);
-		}
-		if (client.add(id, result) != null) {
+
+		if (resource.CouchbaseResource.getClient().add(id, result) != null) {
 			return true;
 		}
 		return false;
 	}
 
 	public User getUser(String id) throws JsonParseException, JsonMappingException, IOException {
-		if (client == null) {
-			client = CouchbaseResource.getClient(config);
-		}
-		Object result = client.get(id);
+
+		Object result = CouchbaseResource.getClient().get(id);
 		ObjectMapper mapper = new ObjectMapper();
 		User user = mapper.readValue(String.valueOf(result), User.class);
 		return user;
